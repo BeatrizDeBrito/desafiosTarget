f4457af5410c893de3d97b50a29cf956a471c5bb
Beatriz de Brito <77403137+BeatrizDeBrito@users.noreply.github.com>
2024-09-14 14:47:17 -0300

    Adiciona questao 2

---
+++ src/FIbonacci.java
@@ -0,0 +1,29 @@
+import java.util.Scanner;
+
+public class FIbonacci {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        int entrada = scanner.nextInt();
+        scanner.close();
+
+        if(verificaFibonacci(entrada)){
+            System.out.println("O número "+ entrada + " pertence á sequência Fibonacci");
+        } else {
+            System.out.println("O número "+ entrada + " não pertence á sequência Fibonacci");
+        }
+    }
+
+    public static boolean verificaFibonacci(int n) {
+        if(n <0){
+            return false;
+        }
+        int a = 0, b = 1;
+        while (a < n) {
+            int temporario = a;
+            a = b;
+            b = temporario + b;
+        }
+        return a == n;
+    }
+}
