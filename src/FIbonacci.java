b7f8cf36d5119bee0040ffe0725d29957c413194
Beatriz de Brito <77403137+BeatrizDeBrito@users.noreply.github.com>
2024-09-14 15:11:26 -0300

    Adiciona mensagem na questao 2

--- src/FIbonacci.java
+++ src/FIbonacci.java
@@ -3,6 +3,7 @@ import java.util.Scanner;
 public class FIbonacci {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
+        System.out.print("Digite um numero: ");
 
         int entrada = scanner.nextInt();
         scanner.close();
